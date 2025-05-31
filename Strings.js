c0a2ab2de899095f257d07f88eeb3820eab5b493
Amd5500gt <jasss.pb03#gmail.com>
2025-05-31 06:30:25 +0530

    yes

---
+++ Strings.js
@@ -0,0 +1,15 @@
+// const name = 'harjeet'
+// const repocount = 7
+
+// //console.log (name + repocount+ 'value') useless method to print string
+
+// console.log(`My name is ${name.} and my repocount is ${repocount}`); //easy and clean method
+
+// //making string object
+
+const gameName = new String ('FreeFire')
+
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.__proto__);
